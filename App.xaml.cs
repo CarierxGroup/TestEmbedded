7cde02a3c64c178373d65d404f42427188b29cea
晚餐骨Dinnerbone <3293876954@qq.com>
2019-10-27 09:00:59 +0800

    233

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestEmbedded
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MainWindowAppContainer.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace TestEmbedded
+{
+    /// <summary>
+    /// MainWindowAppContainer.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindowAppContainer
+    {
+        static void Mainfx(string[] args)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe ";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = false;
+            p.Start();
+            p.StandardInput.WriteLine("start cmd.exe"); //先转到系统盘下
+            p.StandardInput.WriteLine("exit");
+
+            p.WaitForExit();
+            p.Close();
+            p.Dispose();
+
+        }
+        private bool _isLoadSuccess;
+        public MainWindowAppContainer()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindowAppContainer_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (!_isLoadSuccess)
+            {
+                _isLoadSuccess = ctnTest.StartAndEmbedProcess(@"C:\Windows\system32\mspaint.exe");
+            }
+        }
+    }
+}
